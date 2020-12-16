df734440f427040ce4e22c54eead2480ba128824
licc <licc@7moor.com>
2020-12-16 22:47:36 +0800

    new data

--- src/App.vue
+++ src/App.vue
@@ -2,15 +2,20 @@
   <img alt="Vue logo" src="./assets/logo.png" />
   <HelloWorld msg="Hello Vue 3.0 + Vite" />
   jnkndnjkdna
+  {{data1}}
 </template>
 
-<script>
+<script lang="ts">
 import HelloWorld from './components/HelloWorld.vue'
-
 export default {
   name: 'App',
   components: {
     HelloWorld
+  },
+  data () {
+    return {
+      data1: '我是data'
+    }
   }
 }
 </script>
