a6c00b6052e5ca5ead23d694137a2a04cdf0c4ee
qxy_mac <961150665@qq.com>
2020-12-16 23:10:06 +0800

    vue computed

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -8,6 +8,7 @@
   <h1>{{ msgC }}</h1>
   <h1>{{ msgC }}</h1>
   <h1>{{ msgC }}</h1>
+  <h1>{{ msgC }}</h1>
   <button @click="count++">count is: {{ count }}</button>
   <p>Edit <code>components/HelloWorld.vue</code> to test hot module replacement.</p>
 </template>
