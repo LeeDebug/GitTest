9eb82703498d0f57be4d5a01e860fb6a2bf50de9
licc <licc@7moor.com>
2020-12-16 23:04:10 +0800

    vue computed

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,5 +1,6 @@
 <template>
   <h1>{{ msg }}</h1>
+  <h1>{{ msgC }}</h1>
   <button @click="count++">count is: {{ count }}</button>
   <p>Edit <code>components/HelloWorld.vue</code> to test hot module replacement.</p>
 </template>
@@ -13,6 +14,11 @@ export default {
   data() {
     return {
       count: 0
+    }
+  },
+  computed: {
+    msgC () {
+      return this.count * 2
     }
   }
 }
