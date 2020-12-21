17dc83f8a28f783c27c5954abd92ef841b307410
licc <licc@7moor.com>
2020-12-21 23:43:33 +0800

    test

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -3,9 +3,6 @@
   <h1>{{ msgC }}</h1>
   <button @click="count++">count is: {{ count }}</button>
   <p>Edit <code>components/HelloWorld.vue</code> to test hot module replacement.</p>
-  <p>Edit <code>components/HelloWorld.vue</code> to test hot module replacement.</p>
-  <p>Edit <code>components/HelloWorld.vue</code> to test hot module replacement.</p>
-  <p>Edit <code>components/HelloWorld.vue</code> to test hot module replacement.</p>
 </template>
 
 <script>
