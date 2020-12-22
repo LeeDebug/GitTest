e177e0471b5e8341133be45eed1ea89b9ad3e4b0
qducc <961150665@qq.com>
2020-12-22 23:53:55 +0800

    push.sh auto commit

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,7 @@
 <template>
   <img alt="Vue logo" src="./assets/logo.png" />
   <HelloWorld msg="Hello Vue 3.0 + Vite" />
-  jnkndnjkdna
-  {{data1}}
-  {{data2}}
-  <button @click="handleTest()">按钮</button>
+  <!-- <button @click="handleTest()">按钮</button> -->
 </template>
 
 <script lang="ts">
@@ -13,21 +10,11 @@ export default {
   name: 'App',
   components: {
     HelloWorld
-  },
-  data () {
-    return {
-      data1: '我是data'
-    }
-  },
-  computed: {
-    data2 () {
-      return 'dsdadjnsdjnks'
-    }
-  },
-  method: {
-    handleTest () {
-      console.log('2222222222')
-    }
   }
+  // method: {
+  //   handleTest () {
+  //     console.log('2222222222')
+  //   }
+  // }
 }
 </script>
