e67461ff5b649ba48f1efb5d5ac81c1047221304
qducc <961150665@qq.com>
2020-12-22 23:55:52 +0800

    push.sh auto commit

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,8 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png" />
-  <HelloWorld msg="Hello Vue 3.0 + Vite" />
-  <!-- <button @click="handleTest()">按钮</button> -->
+  <!-- <img alt="Vue logo" src="./assets/logo.png" /> -->
+  <!-- <HelloWorld msg="Hello Vue 3.0 + Vite" /> -->
+  {{ data1 }}
+  <button @click="handleTest()">按钮</button>
 </template>
 
 <script lang="ts">
@@ -10,6 +11,12 @@ export default {
   name: 'App',
   components: {
     HelloWorld
+  },
+  setup() {
+    const data1 = 'dsds'
+    return {
+      data1
+    }
   }
   // method: {
   //   handleTest () {
