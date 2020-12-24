317110a5da33c17e1646ab6fc886bd70d390143a
qducc <961150665@qq.com>
2020-12-24 23:59:18 +0800

    push.sh auto commit

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,19 @@
 <template>
-  <!-- <img alt="Vue logo" src="./assets/logo.png" /> -->
-  <!-- <HelloWorld msg="Hello Vue 3.0 + Vite" /> -->
-  {{ data1 }}
-  <button @click="data1++">+1</button>
-  <button @click="handleTest(data1)">按钮</button>
+  <div class="main">
+    <!-- <img alt="Vue logo" src="./assets/logo.png" /> -->
+    <!-- <HelloWorld msg="Hello Vue 3.0 + Vite" /> -->
+    {{ data1 }}
+    <button @click="data1++">+1</button>
+    <button @click="handleTest(data1)">按钮</button>
+    <div class="text">
+      {{reactiveData}}
+    </div>
+    <button @click="handleEdit()">修改reactiveData</button>
+  </div>
 </template>
 
 <script lang="ts">
-import { onBeforeMount, onMounted, ref } from 'vue'
+import { onBeforeMount, onMounted, ref, reactive } from 'vue'
 import HelloWorld from './components/HelloWorld.vue'
 export default {
   name: 'App',
@@ -28,18 +34,33 @@ export default {
     })
 
     const data1:object = ref(2)
-    const handleTest = (v) => {
+    const reactiveData:any = reactive({
+      key1: 233,
+      key2: '你就开电脑开机撒'
+    })
+    function handleTest(v) {
       console.log(23333, v)
+    }
+    function handleEdit() {
+      reactiveData.key1 *= 2
     }
     return {
       data1,
-      handleTest
+      reactiveData,
+      handleTest,
+      handleEdit,
     }
   }
-  // method: {
-  //   handleTest () {
-  //     console.log('2222222222')
-  //   }
-  // }
 }
 </script>
+
+<style scoped>
+.main {
+  border: 1px solid red;
+  width: 800px;
+  height: 300px;
+}
+button {
+  display: block;
+}
+</style>
