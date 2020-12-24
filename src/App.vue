b7f358a5ba363a2026a844563a6de038db624488
qducc <961150665@qq.com>
2020-12-24 23:59:37 +0800

    push.sh auto commit

--- src/App.vue
+++ src/App.vue
@@ -33,7 +33,7 @@ export default {
       console.log('onBeforeMount======')
     })
 
-    const data1:object = ref(2)
+    const data1:object = ref(2512)
     const reactiveData:any = reactive({
       key1: 233,
       key2: '你就开电脑开机撒'
