e50b11109123089c7fb48909c353be8a76f861dd
licc <961150665@qq.com>
2020-12-31 23:35:08 +0800

    haahhaah

--- src/App.vue
+++ src/App.vue
@@ -6,6 +6,9 @@
     <button @click="data1++">+1</button>
     <button @click="handleTest(data1)">按钮2333</button>
     <button @click="handleTest(data1)">按钮2333</button>
+    <button @click="handleTest(data1)">按钮2333</button>
+    <button @click="handleTest(data1)">按钮2333</button>
+    <button @click="handleTest(data1)">按钮2333</button>
     <div class="text">
       {{reactiveData}}
     </div>
