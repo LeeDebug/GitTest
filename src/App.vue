22655b242f9274fe46fbd466a1c45edeed2cfe44
licc <961150665@qq.com>
2020-12-31 23:36:38 +0800

    copy

--- src/App.vue
+++ src/App.vue
@@ -5,12 +5,11 @@
     {{ data1 }}
     <button @click="data1++">+1</button>
     <button @click="handleTest(data1)">按钮2333</button>
-    <button @click="handleTest(data1)">按钮2333</button>
-    <button @click="handleTest(data1)">按钮2333</button>
-    <button @click="handleTest(data1)">按钮2333</button>
-    <button @click="handleTest(data1)">按钮2333</button>
     <div class="text">
       {{reactiveData}}
+    </div>
+    <div class="text">
+      {{reactiveData.key2}}
     </div>
     <button @click="handleEdit()">修改reactiveData</button>
   </div>
