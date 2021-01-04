ca133f4881271d81944a503d4b395f7766a46805
licc <961150665@qq.com>
2021-01-04 12:58:58 +0800

    add webimIndex

--- src/App.vue
+++ src/App.vue
@@ -1,23 +1,12 @@
 <template>
-  <div class="main">
-    <!-- <img alt="Vue logo" src="./assets/logo.png" /> -->
-    <!-- <HelloWorld msg="Hello Vue 3.0 + Vite" /> -->
-    {{ data1 }}
-    <button @click="data1++">+1</button>
-    <button @click="handleTest(data1)">按钮2333</button>
-    <div class="text">
-      {{reactiveData}}
-    </div>
-    <div class="text">
-      {{reactiveData.key2}}
-    </div>
-    <button @click="handleEdit()">修改reactiveData</button>
+  <div>
+    <hello-world />
   </div>
 </template>
 
 <script lang="ts">
-import { onBeforeMount, onMounted, ref, reactive, defineComponent } from 'vue'
-import HelloWorld from './components/HelloWorld.vue'
+import { ref, reactive, defineComponent } from 'vue'
+import HelloWorld from './views/home/index.vue'
 export default defineComponent({
   name: 'App',
   components: {
@@ -26,34 +15,7 @@ export default defineComponent({
   setup() {
     // const store = useStore()
 
-    mounted: {
-      console.log('mounted======')
-    }
-
-    onMounted(() => {
-      console.log('onMounted======')
-    })
-
-    onBeforeMount(() => {
-      console.log('onBeforeMount======')
-    })
-
-    const data1:object = ref(2512)
-    const reactiveData:any = reactive({
-      key1: 233,
-      key2: '你就开电脑开机撒'
-    })
-    function handleTest(v) {
-      console.log(23239333, v)
-    }
-    function handleEdit() {
-      reactiveData.key1 *= 2
-    }
     return {
-      data1,
-      reactiveData,
-      handleTest,
-      handleEdit,
     }
   }
 })
