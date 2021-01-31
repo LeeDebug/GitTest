fee601464887f3f4b1370427f04c756943f013df
licc <961150665@qq.com>
2021-01-31 22:30:00 +0800

    mkfmlkewf

--- src/App.vue
+++ src/App.vue
@@ -3,10 +3,6 @@
     <hello-world class="a" />
     <hello-world class="a" />
     <hello-world class="a" />
-    <hello-world class="a" />
-    <hello-world class="a" />
-    <hello-world class="a" />
-    <hello-world class="a" />
   </div>
 </template>
 
