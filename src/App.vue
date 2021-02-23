f1c7c8723afbc2091504b1cdea1888000e67f001
licc <961150665@qq.com>
2021-02-23 10:07:09 +0800

    njdnjwq

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,6 @@
 <template>
   <div>
     <hello-world class="a" />
-    <hello-world class="a" />
-    <hello-world class="a" />
-    <hello-world class="a" />
-    <hello-world class="a" />
   </div>
 </template>
 
